e045cbcaa83343157dc4925d967f8ddbb58c8335
DharmenderRajput <51106702+DharmenderRajput@users.noreply.github.com>
2020-03-24 18:50:29 +0530

    Update footer.php

--- views/template/footer.php
+++ views/template/footer.php
@@ -1,6 +1,6 @@
-    <footer>
+    <!-- <footer>
         this is some footer part
-    </footer>
+    </footer> -->
     <!-- including all JS Assets from assets folder -->
     <script src="../assets/jquery/jquery-3.4.1.min.js"></script>
     <script src="../assets/bootstrap-3.4.1-dist/js/bootstrap.min.js"></script>

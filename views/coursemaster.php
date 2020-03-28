e831b16f9bcdc02416d3324237d865a890e15adc
DharmenderRajput <51106702+DharmenderRajput@users.noreply.github.com>
2020-03-28 20:37:49 +0530

    Update coursemaster.php

--- views/coursemaster.php
+++ views/coursemaster.php
@@ -17,13 +17,19 @@
 
     include_once("../library/streammaster.php");
 
+    include_once("../library/coursemaster.php");
+
 ?>
 
 <!-- main section of the application -->
 
 <div class="container-fluid" style="height:90vh;border:2px solid black;">
     <div class="row">
-        <div class="col-sm-6" style="height:80vh;border:2px solid black;">
+        <div class="col-sm-5" style="height:80vh;border:2px solid black;">
+
+            <?php
+                if(!isset($_POST['update'])){
+            ?>
 
             <h3 class="page-info">Course Creation</h3>
             <hr>
@@ -54,12 +60,12 @@
                 <br>
                 <div class="input-group">
                     <span class="input-group-addon" id="basic-addon4">Course Duration</span>
-                    <input type="number" class="form-control" name="course_aname" placeholder="Enter Duration" aria-describedby="basic-addon4" required>
-                    <select class="form-control" name="feehead_category" id="" aria-describedby="basic-addon4" required>
+                    <input type="number" class="form-control" name="course_duration" placeholder="Enter Duration" aria-describedby="basic-addon4" required>
+                    <select class="form-control" name="course_period" id="" aria-describedby="basic-addon4" required>
                         <option></option>
-                        <option value="">Hours</option>
-                        <option value="">Months</option>
-                        <option value="">Year</option>
+                        <option value="Hours">Hours</option>
+                        <option value="Months">Months</option>
+                        <option value="Year">Year</option>
                 </select>
                 </div>
                 <br>
@@ -69,22 +75,29 @@
             </form>
 
             <?php
-                 // updating existing stream
-                // if (isset($_POST['update'])) {
-                //     $update = $_POST['update'];
-                //     updateform($update);
-                // }
+                }else{
+                    #implementing updation form
+                    $update = $_POST['update'];
+                    updatecourseform($update);
+                }
             ?>
         </div>
-        <div class="col-sm-6" style="height:80vh;;border:2px solid black;">
-            <!-- display existing stream -->
-            <table class="table">
+        <div class="col-sm-7" style="height:80vh;;border:2px solid black;">
+            <!-- display existing Courses -->
+            <table class="table table-bordered table-striped table-responsive">
                 <tr>
-                    <th>STREAMS</th>
+                    <th colspan="5">Available Courses</th>
+                </tr>
+                <tr class="info">
+                    <th>Stream</th>
+                    <th>Full Name</th>
+                    <th>Abbr Name</th>
+                    <th>Duration</th>
+                    <th>Actions</th>
                 </tr>
                 <?php
                     // fetching existing streams
-                    // fetchstream();
+                    fetchcourses();
                 ?>
             </table>
         </div>
@@ -97,28 +110,49 @@
     // including footer file
     include_once("./template/footer.php");
 
-    // adding new stream
+    // adding new course
     if (isset($_POST['new'])) {
-        # calling createstreame function
-        $stream_name = $_POST['stream_name'];
-        $result = createstream($stream_name);
+        # calling createcourse function
+        # with all required values;
+        $data = array();
+        $data = [
+            "stream" => $_POST['course_stream'],
+            "fname" => $_POST['course_fname'],
+            "aname" => $_POST['course_aname'],
+            "duration" => $_POST['course_duration'],
+            "period" => $_POST['course_period']
+        ];
+        $result = createcourse($data);
         if($result){
             // echo "<script>alert('Hurray');</script>";
-            header("location:./streammaster.php");
+            // header("location:./streammaster.php");
+            echo "<script>window.location.href='./coursemaster.php';</script>";
+
         }else{
             // echo "<script>alert('Opps!');</script>";
         }
     }
 
-    // updating existing stream
-    if (isset($_POST['update_stream'])) {
-        # calling updatestreame function
-        $id = $_POST['stream_id'];
-        $update = $_POST['stream_name'];
-        $result = updatestream($id, $update);
+    // updating existing course
+    if (isset($_POST['update_crs'])) {
+
+        $id = $_POST['update_crs'];
+        # calling updatecrs function
+        $data = array();
+        $data = [
+            "stream" => $_POST['course_stream'],
+            "fname" => $_POST['course_fname'],
+            "aname" => $_POST['course_aname'],
+            "duration" => $_POST['course_duration'],
+            "period" => $_POST['course_period']
+        ];
+        
+        $result = updatecrs($id, $data);
         if($result){
             // echo "<script>alert('Hurray');</script>";
-            header("location:./streammaster.php");
+            // header("location:./streammaster.php");
+            echo "<script>window.location.href='./coursemaster.php';</script>";
+
         }else{
             echo "<script>alert('Opps!');</script>";
         }
@@ -128,10 +162,12 @@
     if (isset($_POST['delete'])) {
         # calling deletestreame function
         $delete = $_POST['delete'];
-        $result = deletestreame($delete);
+        $result = deletecourse($delete);
         if($result){
             // echo "<script>alert('Stream Has Been Successfully Removed!');</script>";
-            header("location:./streammaster.php");
+            // header("location:./streammaster.php");
+            echo "<script>window.location.href='./coursemaster.php';</script>";
+
         }else{
             echo "<script>alert('Opps Some Error has occured.');<script>";
         }

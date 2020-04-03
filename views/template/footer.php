c993c2281781b02a263193ae999461cf53129201
DharmenderRajput <51106702+DharmenderRajput@users.noreply.github.com>
2020-04-03 18:43:37 +0530

    Update footer.php

--- views/template/footer.php
+++ views/template/footer.php
@@ -5,5 +5,6 @@
     <script src="../assets/jquery/jquery-3.4.1.min.js"></script>
     <script src="../assets/bootstrap-3.4.1-dist/js/bootstrap.min.js"></script>
     <script src="../assets/js/main.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
 </body>
 </html>
